315e857b87ab48061c3afb076d1392fa86e786c1
arshiam2 <arshiam2@illinois.edu>
2018-09-21 12:59:49 -0500

    mm-matches

---
+++ index.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const authenticate = require("mm-authenticate")(mongoose);
+const { send } = require("micro");
+const { Script, Match } = require("mm-schemas")(mongoose);
+
+mongoose.connect(process.env.MONGO_URL);
+mongoose.Promise = global.Promise;
+
+module.exports = authenticate(async (req, res) => {
+  const team = req.user;
+  console.log(`${team.name} - Getting matches`);
+  const script = await Script.findById(team.latestScript).exec();
+
+  const matches = await Match.find({ key: { $regex: script.key } }).exec();
+  const wins = matches.map(
+    match =>
+      match.winner === 3
+        ? "tie"
+        : (match.key.split(":")[1] === match.winner && match.winner == 2) ||
+          (match.key.split(":")[1] !== match.winner && match.winner == 1)
+          ? "winner"
+          : "loser"
+  );
+
+  const oponents = matches.map(match =>
+    match.key
+      .replace(":", "")
+      .replace("logs/", "")
+      .replace(script.key, "")
+  );
+
+  let oponentsNames = await Promise.all(
+    oponents.map(oponent => Script.findOne({ key: { $regex: oponent } }))
+  );
+
+  let oponentTeamNames = await Promise.all(
+    oponentsNames.map(
+      oponentsName => Team.findOne({ latestScript: oponentsName.key }).name
+    )
+  );
+
+  return oponentsNames;
+});
