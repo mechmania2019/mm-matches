121eb33daa0d25febb7220835ff9120ee6c34fad
angiecheng <angie.cheng33@gmail.com>
2019-09-19 00:52:51 -0500

    cleaned up code

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// const { promisify } = require("util");
 const mongoose = require("mongoose");
 const authenticate = require("mm-authenticate")(mongoose);
 const { Script, Match, Team } = require("mm-schemas")(mongoose);
@@ -104,59 +103,3 @@ module.exports = authenticate(
     }
   }
 );
-//   router(
-//     get("/matches/:script", async (req, res) => {
-//       const team = req.user;
-//       const script = req.params.script;
-//       console.log(
-//         `${team.name} - Getting competitor scripts against ${script}`
-//       );
-//       const competitors = (await getCompetitors()).filter(
-//         team => team.latestScript && team.latestScript.key !== script
-//       );
-
-//       const matchKeyObjects = competitors.map(team => ({
-//         opponent: team.name,
-//         key: getMatchKey(script, team.latestScript.key)
-//       }));
-
-//       const matchToTeamName = {};
-//       matchKeyObjects.forEach(({ key, opponent }) => {
-//         matchToTeamName[key] = opponent;
-//       });
-
-//       const matches = await Match.find({
-//         key: { $in: matchKeyObjects.map(({ key }) => key) }
-//       }).exec();
-//       return matches.map(m => ({
-//         match: m,
-//         opponent: matchToTeamName[m.key],
-//         result: status(m, script)
-//       }));
-//     }),
-//     get("/:key", async (req, res) => {
-//       const team = req.user;
-//       const key = req.params.key.trim();
-
-//       console.log(`${team.name} - Getting team names ${key}`);
-//       const [s1, s2] = key.slice("logs/".length).split(":");
-//       const scripts = await Promise.all(
-//         [s1, s2].map(s =>
-//           Script.findOne({ key: s })
-//             .populate("owner")
-//             .exec()
-//         )
-//       );
-
-//       console.log(`${team.name} - Got team names`);
-//       console.log(scripts);
-
-//       console.log(`${team.name} - Sending headers`);
-//       res.setHeader("X-team-1", scripts[0].owner.name);
-//       res.setHeader("X-team-2", scripts[1].owner.name);
-
-//       console.log(`${team.name} - Getting logfile ${key}`);
-//       return s3.getObject({ Key: key }).createReadStream();
-//     })
-//   )
-// );
